1c42dc1b8a3fad3565d4bb5c09918b864671c8ea
HuajiMUR233 <13060847725@163.com>
2022-01-20 00:00:29 +0800

    added BanListCommand

--- src/main/java/cn/dancingsnow/timedban/commands/BanListCommand.java
+++ src/main/java/cn/dancingsnow/timedban/commands/BanListCommand.java
@@ -1,25 +1,14 @@
 package cn.dancingsnow.timedban.commands;
 
 import cn.dancingsnow.timedban.Timedban;
-import cn.dancingsnow.timedban.utils.BanPlayer;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
-import com.mojang.brigadier.builder.RequiredArgumentBuilder;
-import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.suggestion.Suggestions;
-import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import com.mojang.brigadier.tree.LiteralCommandNode;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentSkipListSet;
 
 @Singleton
 public class BanListCommand {
@@ -27,53 +16,27 @@ public class BanListCommand {
     @Inject
     private Timedban timedban;
 
-    @Inject
-    private ProxyServer server;
-
     public BanListCommand() {
-        timedban.server.getEventManager().register(timedban, timedban.injector.getInstance(BanCommand.class));
-        timedban.commandManager.register(timedban.injector.getInstance(BanCommand.class).createBrigadierCommand());
+        timedban.server.getEventManager().register(timedban, timedban.injector.getInstance(BanListCommand.class));
+        timedban.commandManager.register(timedban.injector.getInstance(BanListCommand.class).createBrigadierCommand());
     }
 
     public BrigadierCommand createBrigadierCommand() {
         LiteralCommandNode<CommandSource> node = LiteralArgumentBuilder
-                .<CommandSource>literal("tban").requires(commandSource -> commandSource.hasPermission("timedban.admin"))
-                .then(RequiredArgumentBuilder.<CommandSource, String>argument("username", StringArgumentType.string())
-                        .suggests(this::getUsernameSuggestions)
-                        .then(RequiredArgumentBuilder.<CommandSource, Integer>argument("day", IntegerArgumentType.integer())
-                                .executes(context -> {
-                                    String name = context.getArgument("username", String.class);
-                                    Integer day = context.getArgument("day", Integer.class);
-                                    BanPlayer banPlayer = new BanPlayer(day, "I don't know");
-                                    timedban.banList.addBanPlayer(name, banPlayer);
-                                    context.getSource().sendMessage(Component.text("封禁成功").color(NamedTextColor.GREEN));
-                                    return 1;
-                                })
-                                .then(RequiredArgumentBuilder.<CommandSource, String>argument("reason", StringArgumentType.string())
-                                        .executes(context -> {
-                                            String name = context.getArgument("username", String.class);
-                                            String reason = context.getArgument("reason", String.class);
-                                            Integer day = context.getArgument("day", Integer.class);
-                                            BanPlayer banPlayer = new BanPlayer(day, reason);
-                                            timedban.banList.addBanPlayer(name, banPlayer);
-                                            context.getSource().sendMessage(Component.text("封禁成功").color(NamedTextColor.GREEN));
-                                            return 1;
-                                        }))))
+                .<CommandSource>literal("tbanlist").requires(commandSource -> commandSource.hasPermission("timedban.banlist"))
+                .executes(context -> replyMessage(context.getSource()))
                 .build();
         return new BrigadierCommand(node);
     }
 
-    public CompletableFuture<Suggestions> getUsernameSuggestions(final CommandContext<CommandSource> context, final SuggestionsBuilder builder) {
-        ConcurrentSkipListSet<String> playerSet = new ConcurrentSkipListSet<>();
-        server.getAllPlayers().forEach(player -> playerSet.add(player.getUsername()));
-        playerSet.forEach(
-                username -> {
-                    if (username.contains(builder.getRemaining())) {
-                        builder.suggest(username);
-                    }
+    private int replyMessage(CommandSource src) {
+        src.sendMessage(Component.text("以下玩家已被封禁："));
+        timedban.banList.getConfig().forEach(
+                (name, banPlayer) -> {
+                    src.sendMessage(Component.text(name+" "+"解封时间："+banPlayer.getUnBanTimeStr()+" 原因："+banPlayer.getReason()));
                 }
         );
-        return builder.buildFuture();
+        return 1;
     }
 
 }
